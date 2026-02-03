f46cb2f9a7d7fed1f994127b3402cefdc148d7cf
Pradeepjaggumanthri <kumar143pradeep@gmail.com>
2026-02-03 17:11:56 +0530

    issues connecting

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Routes
 app.use("/api/birthdays", require("./routes/birthdayRoutes"));
 
 const PORT = process.env.PORT || 5000;
