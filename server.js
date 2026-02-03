b3f89a037f581a79a0aa2e575727e2f960810478
Pradeepjaggumanthri <kumar143pradeep@gmail.com>
2026-02-03 16:30:55 +0530

    changed server small point

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-const birthdayRoutes = require("./routes/birthdayRoutes");
-app.use("/api/birthdays", birthdayRoutes);
-
-// MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Render requires dynamic PORT
+app.use("/api/birthdays", require("./routes/birthdayRoutes"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
