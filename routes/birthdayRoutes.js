bb02130a6a5fe6a9937df9ce3dd2a18be1bdf5b9
Pradeepjaggumanthri <kumar143pradeep@gmail.com>
2026-02-04 19:29:01 +0530

    changed for add edit and delete any control with text

--- routes/birthdayRoutes.js
+++ routes/birthdayRoutes.js
@@ -2,20 +2,33 @@ const express = require("express");
 const router = express.Router();
 const Birthday = require("../models/Birthday");
 
-/* CREATE */
-router.post("/", async (req, res) => {
-  try {
-    const birthday = await Birthday.create(req.body);
-    res.json(birthday);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+// GET all birthdays
+router.get("/", async (req, res) => {
+  const data = await Birthday.find();
+  res.json(data);
 });
 
-/* GET ALL */
-router.get("/", async (req, res) => {
-  const birthdays = await Birthday.find().sort({ createdAt: -1 });
-  res.json(birthdays);
+// ADD birthday
+router.post("/", async (req, res) => {
+  const newBirthday = new Birthday(req.body);
+  await newBirthday.save();
+  res.json(newBirthday);
+});
+
+// DELETE birthday
+router.delete("/:id", async (req, res) => {
+  await Birthday.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+// UPDATE birthday
+router.put("/:id", async (req, res) => {
+  const updated = await Birthday.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(updated);
 });
 
 module.exports = router;
